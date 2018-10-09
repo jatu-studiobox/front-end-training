e946b279dddec615e5c738f18fa5c7bcb16d8c81
jatu-studiobox <tj_yai@hotmail.com>
2018-10-09 23:53:06 +0700

    Day 7 edit Homework 4 add radius border to card item

--- day7/cardmatchgame/src/CardNode.js
+++ day7/cardmatchgame/src/CardNode.js
@@ -7,7 +7,7 @@ class CardNode extends Component {
 
         return (
             <Card key={this.props.id}
-                style={{ backgroundColor : show ? card.color : 'grey', cursor : 'pointer', width : 120, height : 150}} 
+                style={{ backgroundColor : show ? card.color : 'grey', cursor : 'pointer', width : 120, height : 150, borderRadius : 15}} 
                 onClick={() => onFlip(card.id)}>&nbsp;</Card>
         );
     }
