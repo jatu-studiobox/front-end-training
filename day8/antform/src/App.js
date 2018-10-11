466b1d5e55d84b4a532fffccab6c80de22875ade
jatu-studiobox <tj_yai@hotmail.com>
2018-10-11 17:26:14 +0700

    Day 8 add homework 3 AntDesign Validate Form

---
+++ day8/antform/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import RegisterUser from './RegisterUser';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <RegisterUser />
+      </div>
+    );
+  }
+}
+
+export default App;
